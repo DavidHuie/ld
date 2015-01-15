f887fd6a04283ecbd9450c29e7312b8b6b97610a
David Huie <dahuie@gmail.com>
2015-01-15 15:59:04 -0800

    Add man

--- build/scripts/build.sh
+++ build/scripts/build.sh
@@ -5,7 +5,7 @@ set -ex
 apt-get update -y
 apt-get upgrade -y
 
-apt-get install -y zsh tmux git curl ipcalc
+apt-get install -y zsh tmux git curl ipcalc man
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
